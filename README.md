86f1c8f7c2b7715fe02e4e6067593643852ecc11
Fungie <fergusa@tcd.ie>
2016-12-27 15:10:29 +0000

    adding pdf report

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Exploring_baby_names
+# Exploring Baby Names
+
+Exploratory data analysis on US baby names (1910-2015)
+
+The data can be downloaded from:
+
+http://www.ssa.gov/oact/babynames/state/namesbystate.zip
--- script.R
+++ script.R
@@ -165,5 +165,8 @@ famous_name_check <- function(df, name_lookup = "Jackie"){
 
 famous_name_check(baby_names, name_lookup = "Jackie")
 famous_name_check(baby_names, name_lookup = "Marilyn")
+famous_name_check(baby_names, name_lookup = "Steven")
+
+
 
 
